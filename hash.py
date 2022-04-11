2c35da9b05b2ea7e669734072f250a6270e63461
m221316 <63674843+m221316@users.noreply.github.com>
2022-04-11 13:55:08 -0400

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,9 +1,26 @@
 import os;
+import hashlib
 
+skip = ['/dev', '/proc', '/run', '/sys', 'tmp', '/var/lib', '/var/run']
 dir= "/"
+flag = 0
+
+while(flag == 0):
 
 
-for filename in os.scandir(dir):
-  if filename.is_file():
-    print(filename.path)
+  for filename in os.scandir(dir):
+    if filename.is_file():
+      f = open('files.txt', 'r')
+      contents = f.read()
+      f.close()
+      if filename not in contents:
+        
+        f1 = open('files.txt', 'a')
+        f1.write(filename + "\n")
+
+  for name in os.scandir(dir):
+    if os.path.isdir(name):
+      dir = name
+      
+  
 
